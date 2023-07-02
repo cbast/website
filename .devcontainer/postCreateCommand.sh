548cd9e71ffcbd407e0b5bc0bd66db0340304942
Sebastien Frigon <sebastien@frigon.xyz>
2023-07-02 19:31:47 +0000

    Add autocompletion in dev container

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname "${BASH_SOURCE[0]}")
+
+${SCRIPT_PATH}/setup/bash-autocomplete.sh
---
+++ .devcontainer/setup/bash-autocomplete.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+SCRIPT_PATH=$(dirname "${BASH_SOURCE[0]}")
+
+echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
